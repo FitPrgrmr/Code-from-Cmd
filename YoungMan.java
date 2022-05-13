3663489f2c6aa73ef7a9045c43bf1ec784a2b576
Jimmeh <105396627+FitPrgrmr@users.noreply.github.com>
2022-05-13 22:42:29 +0200

    First .java file commit
    YoungMan.java file is my first .java file compiled from the cmd line and commited to github.

---
+++ YoungMan.java
@@ -0,0 +1,32 @@
+/**
+*@author Jimmeh Camara
+*/
+import java.util.*;
+public class YoungMan{
+	public static void main(String[] args){
+	
+ 	Person individual1 = new Person();	
+	individual1.setName("Jimmeh");
+	System.out.println(individual1.getName() + " Yes!!! It Works! :)");
+	
+   }	
+}
+class Person{
+	
+	int age;
+	String name;
+	String eyeColour;
+
+	public void setName(String newName){
+	this.name = newName;
+	}
+	public String getName(){
+	//System.out.println(name + " Yes! it works");
+	return name;
+	}
+}
+
+
+
+
+
